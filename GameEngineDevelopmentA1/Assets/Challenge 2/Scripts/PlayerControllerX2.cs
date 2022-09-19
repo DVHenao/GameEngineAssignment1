8a401b8da98eef54359c9708859638c8e0a77e91
DVHenao <ehenao094@gmail.com>
2022-09-19 18:43:15 -0400

    Assignment 1 part 2 complete!

---
+++ GameEngineDevelopmentA1/Assets/Challenge 2/Scripts/PlayerControllerX2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX2 : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float SpawnDelay = 1.0f;
+    private float counter = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += 0.01f;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (counter > 0.5f)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                counter = 0;
+            }
+            else
+                Debug.Log("too early!");
+        }
+    }
+}
---
+++ GameEngineDevelopmentA1/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -30;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnInterval = Random.Range(3, 6);
+        StartCoroutine(SpawnRandomBall());
+    }
+
+    // Spawn random ball at random x position at top of play area
+    IEnumerator SpawnRandomBall()
+    {
+
+        while (true)
+        {         // Generate random ball index and random spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+            spawnInterval = Random.Range(3, 6);
+            yield return new WaitForSeconds(spawnInterval);
+
+
+            int Rand = Random.Range(0, 3);
+            Debug.Log(spawnInterval);
+            // instantiate ball at random spawn location
+
+            switch (Rand) // the only reason this is in a switch statement is because setting the array element never worked out for me
+            {
+                case 0:
+                    Debug.Log("Blue");
+                    Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+                    break;
+                case 1:
+                    Debug.Log("Green");
+                    Instantiate(ballPrefabs[1], spawnPos, ballPrefabs[1].transform.rotation);
+                    break;
+                case 2:
+                    Debug.Log("Red");
+                    Instantiate(ballPrefabs[2], spawnPos, ballPrefabs[2].transform.rotation);
+                    break;
+
+
+
+            }
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        spawnInterval = Random.Range(3, 6);
+       
+    }
+}
